15dc75b4908d334313f90bcdb2032110b11ec61e
Eugene <kimeugene65@gmail.com>
2022-03-23 01:29:52 +0900

    learn-nuxt3

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,12 +9,12 @@ module.exports = {
     requireConfigFile: false,
   },
   extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
-  plugins: ['prettier'],
+  plugins: [],
   // add your custom rules here
   rules: {
     'vue/multi-word-component-names': 'off',
-    camelcase: 'off',
-    'no-undef': 'off',
-    'no-console': 'off',
+    // camelcase: 'off',
+    // 'no-undef': 'off',
+    // 'no-console': 'off',
   },
 }
--- pages/main.vue
+++ pages/main.vue
@@ -1,11 +1,44 @@
 <template>
     <div>
         <p>메인 페이지입니다.</p>
+        <div>
+            <ul>
+                <li v-for="product in products" :key="product.id">
+                    <img :src="product.imageUrl" alt="product.name">
+                    <p>{{ product.name }}</p>
+                    <p>{{ product.price }}</p>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 <script>
+import axios from 'axios';
+
 export default {
-    
+    // pages 폴더 아래에 위치하는 컴포넌트에만 제공되는 속성
+    // 컴포넌트가 생성되기 이전에 호출: asyncData >>> this 접근 불가
+    async asyncData() {
+        const response = await axios.get('http://localhost:3000/products')
+        console.log(response)
+        const products = response.data.map(item => {
+            return {
+                ...item,
+                imageUrl: `${item.imageUrl}?random=${Math.random()}`
+            }
+        })
+        return { products }
+    },
+    /* data() {
+        return {
+            products: [],
+        }
+    },
+    async created() {
+        const response = await axios.get('http://localhost:3000/products')
+        console.log(response)
+        this.products = response.data;
+    }, */
 }
 </script>
 <style>
