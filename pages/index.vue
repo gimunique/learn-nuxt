28670360b202c26c5233b4092de686a2f114fa31
eugene kim <eugene@nexon.co.kr>
2022-03-30 18:46:28 +0900

    page develop

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,100 @@
-<template lang="">
+<template>
+<div class="app">
+	<main>
 	<div>
-		<p>넉스트 시작하기</p>
+		<input type="text">
 	</div>
+	<ul>
+		<li class="item flex" 
+			v-for="product in products" 
+			:key="product.id" 
+			@click="moveToDetailPage(product.id)"
+		>
+		<img class="product-image" :src="product.imageUrl" :alt="product.name">
+		<p>{{ product.name }}</p>
+		<span>{{ product.price }}</span>
+		</li>
+	</ul>
+	</main>
+	<!-- <ProductList></ProductList> -->
+</div>
 </template>
 <script>
-  export default {
+import axios from 'axios';
+// import ProductList from '~/components/ProductList.vue'
 
-  }
+export default {
+	// components: { ProductList }
+	// [pages 폴더 아래에 위치하는 컴포넌트에만] 제공되는 속성
+	// 컴포넌트가 생성되기 이전에 호출: asyncData >>> this 접근 불가
+	// asyncData는 뷰 컴포넌트에서 제공하는 인스턴스 옵션 속성
+	async asyncData() {
+	const response = await axios.get('http://localhost:3000/products')
+	// console.log(response)
+	const products = response.data.map(item => {
+			return {
+				...item,
+				imageUrl: `${item.imageUrl}?random=${Math.random()}`
+			}
+		})
+		return { products }
+	},
+	methods: {
+		moveToDetailPage(id) {
+			console.log(id)
+			// nuxt는 라우터를 내부적으로 품고 있음
+			this.$router.push(`detail/${id}`)
+		}
+	},
+	/* data() {
+		return {
+			products: [],
+		}
+	},
+	async created() {
+		const response = await axios.get('http://localhost:3000/products')
+		console.log(response)
+		this.products = response.data;
+	}, */
+}
 
 </script>
-<style lang="">
+<style scoped>
+.flex {
+	display: flex;
+	justify-content: center;
+}
 
-</style>
+.item {
+	display: inline-block;
+	width: 400px;
+	height: 300px;
+	text-align: center;
+	margin: 0 0.5rem;
+	cursor: pointer;
+}
+
+.product-image {
+	width: 400px;
+	height: 250px;
+}
+
+.app {
+	position: relative;
+}
+
+.cart-wrapper {
+	position: sticky;
+	float: right;
+	bottom: 50px;
+	right: 50px;
+}
+
+.cart-wrapper .btn {
+	display: inline-block;
+	height: 40px;
+	font-size: 1rem;
+	font-weight: 500;
+}
+
+</style>  
--- pages/main.vue
+++ pages/main.vue
@@ -1,38 +1,41 @@
 <template>
-    <div>
-        <p>메인 페이지입니다.</p>
-        <div>
-            <ul>
-                <li v-for="product in products" :key="product.id">
-                    <img :src="product.imageUrl" alt="product.name">
-                    <p>{{ product.name }}</p>
-                    <p>{{ product.price }}</p>
-                </li>
-            </ul>
-        </div>
-        <!-- <ProductList></ProductList> -->
-    </div>
+  <div class="app">
+    <main>
+      <div>
+        <input type="text">
+      </div>
+      <ul>
+        <li class="item flex" v-for="product in products" :key="product.id">
+          <img class="product-image" :src="product.imageUrl" :alt="product.name">
+          <p>{{ product.name }}</p>
+          <span>{{ product.price }}</span>
+        </li>
+      </ul>
+    </main>
+    <!-- <ProductList></ProductList> -->
+  </div>
 </template>
 <script>
-import axios from 'axios';
-// import ProductList from '~/components/ProductList.vue'
+  import axios from 'axios';
+  // import ProductList from '~/components/ProductList.vue'
 
-export default {
+  export default {
     // components: { ProductList }
-    // pages 폴더 아래에 위치하는 컴포넌트에만 제공되는 속성
+    // [pages 폴더 아래에 위치하는 컴포넌트에만] 제공되는 속성
     // 컴포넌트가 생성되기 이전에 호출: asyncData >>> this 접근 불가
     // asyncData는 뷰 컴포넌트에서 제공하는 인스턴스 옵션 속성
     async asyncData() {
-        const response = await axios.get('http://localhost:3000/products')
-        // console.log(response)
-        const products = response.data.map(item => {
-            console.log(item);
-            return {
-                ...item,
-                imageUrl: `${item.imageUrl}?random=${Math.random()}`
-            }
-        })
-        return { products }
+      const response = await axios.get('http://localhost:3000/products')
+      // console.log(response)
+      const products = response.data.map(item => {
+        return {
+          ...item,
+          imageUrl: `${item.imageUrl}?random=${Math.random()}`
+        }
+      })
+      return {
+        products
+      }
     },
     /* data() {
         return {
@@ -44,8 +47,45 @@ export default {
         console.log(response)
         this.products = response.data;
     }, */
-}
+  }
+
 </script>
-<style>
-    
+<style scoped>
+  .flex {
+    display: flex;
+    justify-content: center;
+  }
+
+  .item {
+    display: inline-block;
+    width: 400px;
+    height: 300px;
+    text-align: center;
+    margin: 0 0.5rem;
+    cursor: pointer;
+  }
+
+  .product-image {
+    width: 400px;
+    height: 250px;
+  }
+
+  .app {
+    position: relative;
+  }
+
+  .cart-wrapper {
+    position: sticky;
+    float: right;
+    bottom: 50px;
+    right: 50px;
+  }
+
+  .cart-wrapper .btn {
+    display: inline-block;
+    height: 40px;
+    font-size: 1rem;
+    font-weight: 500;
+  }
+
 </style>
